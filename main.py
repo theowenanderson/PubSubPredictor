532845342b582b682d86a780cb77eb0b3f921a51
Owen <owena@knights.ucf.edu>
2020-05-30 00:11:23 -0400

    test

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from twitterscraper import query_tweets
 import pandas as pd
 
 df2 = pd.read_json (r'/Users/owena/PubSubPredictor/tweets.json')
-df1 = df2[df2['text'].str.contains("Yes!")] 
+
+df1 = df2[df2['text'].str.contains("Yes!")]
